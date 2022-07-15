0d9f61f55492a9699b786a65377128a9f4cc9035
Paul Vonder Haar <pvonhaar@gmail.com>
2022-07-15 17:43:11 -0500

    Uploading all the files

---
+++ Assets/Scenes/Player_Controller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Controller : MonoBehaviour
+{       
+        public float moveSpeed=5f;
+        public InputAction playerControls;
+        public Rigidbody2D rb;
+        public Vector2 moveDirection = Vector2.zero;
+
+    private void Awake()
+    { 
+
+        playerControls.Enable();
+    }
+    private void onDisable()
+    {   Debug.Log("Disabeled");
+        playerControls.Disable();
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = playerControls.ReadValue<Vector2>();
+        transform.Translate(moveDirection * moveSpeed);
+
+    }
+
+}
